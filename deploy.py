747c7d38ebc5d66627dda6b389034acf42eb5a17
opeoluwafolarin <mariafolarin.o@gmail.com>
2022-08-18 05:34:09 +0000

    version2

--- deploy.py
+++ deploy.py
@@ -1,5 +1,8 @@
 #deploying a python web application 
-echo "hello world" 
+#echo "hello world" 
 #shell script
-python ( 'hello world' ) 
+#python ( 'hello world' ) 
 #python script
+
+print('hello world' )
+
